c3bfaf866249ec80657bdc060b22a432cb4e9f60
adhil <adhil.mhdk28@gmail.com>
2024-09-06 06:32:00 +0530

    fixes

--- app/(client)/test/stock/sections/Summary/Summary.jsx
+++ app/(client)/test/stock/sections/Summary/Summary.jsx
@@ -1,6 +1,11 @@
+import { Select } from "@/components/select/page";
 import Graph from "./Graph";
+import { useState } from "react";
 
 export default function SummarySection() {
+
+    const [openCompare, setOpenCompare] = useState(false);
+
     return <div className="text-xl flex flex-col">
         <span className="text-[#D7B257C9] font-extrabold">What's the Game Plan?</span>
         <p>
@@ -20,10 +25,14 @@ export default function SummarySection() {
                 </li>
             </ul>
         </div>
-        <div className="w-full">
-            
+        <div className="w-full mt-5 flex justify-between">
+            <div>
+                <span className="text-[#D7B257C9] font-extrabold">Metrics</span>
+            </div>
+            <Select onSelect={(i) => setOpenCompare(i === "Compare")} options={['Current', 'Compare']} className={'text-sm w-32'} />
         </div>
-        <div className="flex w-full gap-x-5 justify-between rounded-lg my-5 text-white">
+
+        <div className="flex w-full gap-x-5 justify-between rounded-lg mt-5 text-white">
             {/* Buy Section */}
             <div className="flex flex-1 flex-col justify-center bg-[#393939] p-5 rounded">
                 <div className="flex items-center">
@@ -50,6 +59,34 @@ export default function SummarySection() {
             </div>
 
         </div>
+
+        {openCompare && <div className="animate-expand-y flex w-full gap-x-5 justify-between rounded-lg my-5 text-white">
+            {/* Buy Section */}
+            <div className="flex flex-1 flex-col justify-center bg-[#2B2A2A] p-5 rounded">
+                <div className="flex items-center">
+                    <div className="h-6 w-2 rounded-full bg-gradient-to-r from-[#D7B257C9] to-[#715E2E47]"></div>
+                    <div className="text-sm ml-2">Buy:&nbsp;&nbsp;359.00</div>
+                </div>
+                <div className="text-blue-400 text-sm mt-2">-3.05 -0.84%</div>
+            </div>
+            {/* TARGET Section */}
+            <div className="flex flex-1 flex-col justify-center bg-[#2B2A2A] p-5 rounded">
+                <div className="flex items-center">
+                    <div className="h-6 w-2 rounded-full bg-gradient-to-r from-[#D7B257C9] to-[#715E2E47]"></div>
+                    <div className="text-sm ml-2">Target:&nbsp;&nbsp;370.00</div>
+                </div>
+                <div className="text-success text-sm mt-2">+10.10 +2.81%</div>
+            </div>
+            {/* STOP LOSS Section */}
+            <div className="flex flex-1 flex-col justify-center bg-[#2B2A2A] p-5 rounded">
+                <div className="flex items-center">
+                    <div className="h-6 w-2 rounded-full bg-gradient-to-r from-[#D7B257C9] to-[#715E2E47]"></div>
+                    <div className="text-sm ml-2">Buy:&nbsp;&nbsp;359.00</div>
+                </div>
+                <div className="text-danger text-sm mt-2">-4.90 -1.36%</div>
+            </div>
+
+        </div>}
 
         <span className="text-[#D7B257C9] font-extrabold">Performance:</span>
         <Graph />
