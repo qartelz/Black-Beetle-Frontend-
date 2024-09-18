d7160b6680e5ca0c22522680a36551764bd08de3
sidharthpankajakshan94 <macbookpro@MacBooks-MacBook-Pro.local>
2024-09-18 18:13:25 +0530

    api updated

--- app/(client)/admin/page.jsx
+++ app/(client)/admin/page.jsx
@@ -35,9 +35,9 @@ const TradeForm = ({ TradeData, onChange, onSave, onClose, isEditing }) => {
       <div className="text-black uppercase text-xl mb-2">
         <input
           type="text"
-          name="acronym"
-          value={TradeData.acronym}
-          onChange={(e) => handleChange("acronym", e.target.value)}
+          name="stock_index"
+          value={TradeData.stock_index}
+          onChange={(e) => handleChange("stock_index", e.target.value)}
           className="text-lg font-oswald border border-black rounded-lg p-2"
           placeholder="Stock Name"
           disabled={isEditing}
@@ -84,25 +84,25 @@ const TradeForm = ({ TradeData, onChange, onSave, onClose, isEditing }) => {
       <div className="flex flex-col sm:flex-row mt-3 mb-4 space-x-2">
         <input
           type="number"
-          name="buy_price"
-          value={TradeData.buy_price}
-          onChange={(e) => handleChange("buy_price", e.target.value)}
+          name="buy"
+          value={TradeData.buy}
+          onChange={(e) => handleChange("buy", e.target.value)}
           className="text-lg border border-black rounded-lg p-2 w-full"
           placeholder="Entry Price"
         />
         <input
           type="number"
-          name="stop_loss"
-          value={TradeData.stop_loss}
-          onChange={(e) => handleChange("stop_loss", e.target.value)}
+          name="sl"
+          value={TradeData.sl}
+          onChange={(e) => handleChange("sl", e.target.value)}
           className="text-lg border border-black rounded-lg p-2 w-full"
           placeholder="Stop Loss"
         />
         <input
           type="number"
-          name="target_price"
-          value={TradeData.target_price}
-          onChange={(e) => handleChange("target_price", e.target.value)}
+          name="target"
+          value={TradeData.target}
+          onChange={(e) => handleChange("target", e.target.value)}
           className="text-lg border border-black rounded-lg p-2 w-full"
           placeholder="Target"
         />
@@ -110,19 +110,19 @@ const TradeForm = ({ TradeData, onChange, onSave, onClose, isEditing }) => {
 
       <div className="mb-4 gap-2 flex">
         <DatePicker
-          name="expire"
-          selected={TradeData.expire}
+          name="expiry_date"
+          selected={TradeData.expiry_date}
           disabled={isEditing}
-          onChange={(date) => onChange("expire", date)}
+          onChange={(date) => onChange("expiry_date", date)}
           className="p-2  rounded  border border-gray-300"
           placeholderText="Select Expiry"
           dateFormat="MM/dd/yyyy"
         />
 
         <select
-          name="trade_status"
-          value={TradeData.trade_status}
-          onChange={(e) => handleChange("trade_status", e.target.value)}
+          name="status"
+          value={TradeData.status}
+          onChange={(e) => handleChange("status", e.target.value)}
           disabled={isEditing}
           className="text-lg font-oswald border border-black rounded-lg p-2"
         >
@@ -136,16 +136,16 @@ const TradeForm = ({ TradeData, onChange, onSave, onClose, isEditing }) => {
       </div>
 
       <ReactQuill
-        value={TradeData.analysis_bull}
+        value={TradeData.bull_scenario}
         readOnly={isEditing}
-        onChange={(value) => handleChange("analysis_bull", value)}
+        onChange={(value) => handleChange("bull_scenario", value)}
         placeholder="Enter your Bullish Analysis here..."
         className="w-full text-black rounded mb-10"
       />
 
       <ReactQuill
-        value={TradeData.analysis_bear}
-        onChange={(value) => handleChange("analysis_bear", value)}
+        value={TradeData.bear_scenario}
+        onChange={(value) => handleChange("bear_scenario", value)}
         readOnly={isEditing}
         placeholder="Enter your Bearish Analysis here..."
         className="w-full text-black rounded"
@@ -176,16 +176,16 @@ export default function Page() {
 
 
   const [tempData, setTempData] = useState({
-    acronym: "",
+    stock_index: "",
     segment: "",
     trade_type: "",
-    trade_status: "",
-    analysis_bull: "",
-    analysis_bear: "",
-    buy_price: "",
-    stop_loss: "",
-    target_price: "",
-    expire: "",
+    status: "",
+    bull_scenario: "",
+    bear_scenario: "",
+    buy: "",
+    sl: "",
+    target: "",
+    expiry_date: "",
   });
   const [datas, setDatas] = useState([]);
   const [openCreatePopup, setOpenCreatePopup] = useState(false);
@@ -195,7 +195,8 @@ export default function Page() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("/api/tradeData"); // replace with your backend API
+        const response = await axios.get("http://127.0.0.1:8000/admin-side/trades/"); // replace with your backend API
+        console.log(response.data)
         setDatas(response.data); // Store fetched data into the state
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -214,14 +215,14 @@ export default function Page() {
   const handleSave =  async () => {
     // Ensure that required fields are not empty
     if (
-      tempData.acronym.trim() &&
+      tempData.stock_index.trim() &&
       tempData.segment.trim() &&
       tempData.trade_type.trim() &&
-      tempData.trade_status.trim() &&
-      tempData.buy_price &&
-      tempData.stop_loss &&
-      tempData.target_price &&
-      tempData.expire
+      tempData.status.trim() &&
+      tempData.buy &&
+      tempData.sl &&
+      tempData.target &&
+      tempData.expiry_date
     ) {
       
       let updatedDatas;
@@ -237,7 +238,7 @@ export default function Page() {
         updatedDatas = [tempData, ...datas];
       }
       try {
-        const response = await axios.post("/api/tradeData", updatedDatas); // replace with your API endpoint
+        const response = await axios.post("http://127.0.0.1:8000/admin-side/create-trade/", updatedDatas); // replace with your API endpoint
         setDatas(response.data); // Update state with response
       } catch (error) {
         console.error("Error saving data:", error);
@@ -261,16 +262,16 @@ export default function Page() {
   useEffect(() => {
     if (openCreatePopup && !isEditing) {
       setTempData({
-        acronym: "",
-        segment: "",
+        stock_index: "",
+        segment: "",     
         trade_type: "", 
-        trade_status: "",
-        analysis_bull: "",
-        analysis_bear: "",
-        buy_price: "",
-        stop_loss: "",
-        target_price: "",
-        expire: "",
+        status: "",
+        bull_scenario: "",
+        bear_scenario: "",
+        buy: "",
+        sl: "",
+        target: "",
+        expiry_date: "",
       });
     }
   }, [openCreatePopup, isEditing]);
@@ -312,16 +313,16 @@ export default function Page() {
         onClick={() => {
           // Reset tempData immediately before opening the modal
           setTempData({
-            acronym: "",
+            stock_index: "",
             segment: "",
             trade_type: "",
-            trade_status: "",
-            analysis_bull: "",
-            analysis_bear: "",
-            buy_price: "",
-            stop_loss: "",
-            target_price: "",
-            expire: "",
+            status: "",
+            bull_scenario: "",
+            bear_scenario: "",
+            buy: "",
+            sl: "",
+            target: "",
+            expiry_date: "",
           });
 
           // Open the modal
@@ -334,7 +335,7 @@ export default function Page() {
       </Button>
 
       {datas.length > 0 && (
-        <div className="w-full flex flex-col bg-primary p-5 rounded-lg mt-5">
+        <div className="w-full flex flex-col bg-primary text-white p-5 rounded-lg mt-5">
           <div className="flex mb-10 items-center justify-between">
             <div></div>
           </div>
@@ -363,8 +364,8 @@ export default function Page() {
                     key={i}
                   >
                     <td className="p-3 rounded-l-lg">
-                      {data.acronym
-                        ? data.acronym.toUpperCase().replace(/_/g, " ")
+                      {data.stock_index
+                        ? data.stock_index.toUpperCase().replace(/_/g, " ")
                         : "N/A"}
                     </td>
                     <td className="p-3">
@@ -375,17 +376,17 @@ export default function Page() {
                         ? data.trade_type.toUpperCase().replace(/_/g, " ")
                         : "N/A"}
                     </td>
-                    <td className="p-3">{data.buy_price}</td>
-                    <td className="p-3">{data.stop_loss}</td>
-                    <td className="p-3">{data.target_price}</td>
+                    <td className="p-3">{data.buy}</td>
+                    <td className="p-3">{data.sl}</td>
+                    <td className="p-3">{data.target}</td>
 
                     <td className="p-3">
-                      {data.expire ? data.expire.toLocaleDateString() : "N/A"}
+                      { data.expiry_date}
                     </td>
 
                     <td className="p-3">
-                      {data.trade_status
-                        ? data.trade_status.toUpperCase().replace(/_/g, " ")
+                      {data.status
+                        ? data.status.toUpperCase().replace(/_/g, " ")
                         : "N/A"}
                     </td>
                     <td className="p-3 ">
